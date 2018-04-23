9b7b464ad78730ddb09b0f90d311c66ff61f110b
Thiago Sousa da Costa <thiago.ssousa@ctis.com.br>
2018-04-23 11:18:56 -0300

    Configurando o Swagger 2 - https://swagger.io/

---
+++ src/main/java/com/brasilia/teatro/api/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.brasilia.teatro.api.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {                                    
+    @Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build()
+          .apiInfo(apiInfo());                                           
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+          "Teatro API", 
+          "Documentation.", 
+          "1.0", 
+          "Terms of service", 
+          new Contact("Thiago Sousa and Lucas Farias", "https://github.com/thigo08/teatro-api", "thsousa8@hotmail.com"), 
+          "License of API", "http://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+   }
+}
