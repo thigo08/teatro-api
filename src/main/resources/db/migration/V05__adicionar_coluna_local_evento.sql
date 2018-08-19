9acaaa0dfb163149f7dfcebb5b7d15a0371a3fa2
thigo08 <thsousa8@hotmail.com>
2018-08-18 22:25:35 -0300

    'Update_sql'

--- src/main/resources/db/migration/V05__adicionar_coluna_local_evento.sql
+++ src/main/resources/db/migration/V05__adicionar_coluna_local_evento.sql
@@ -1,11 +1,11 @@
 ALTER TABLE local add column latitude VARCHAR(25);
 ALTER TABLE local add column longitude VARCHAR(25);
-ALTER TABLE evento add column imagem VARCHAR(100);
+ALTER TABLE evento add column imagem VARCHAR(400);
 
 UPDATE local SET latitude = '-15.8261884', longitude = '-47.9257462' WHERE id = 1;
 UPDATE local SET latitude = '-15.818182', longitude = '-47.9243037' WHERE id = 2;
 UPDATE local SET latitude = '-15.8444066', longitude = '-48.0360628' WHERE id = 3;
 UPDATE local SET latitude = '-15.8367017', longitude = '-47.9166374' WHERE id = 4;
-UPDATE evento SET imagem = 'http://www.barbixas.com.br/2017/embrevesimg.jpg' WHERE id = 1;
-UPDATE evento SET imagem = 'http://www.desfrutecultural.com.br/wp-content/uploads/2015/02/G7-divulga%C3%A7ao.jpg' WHERE id = 2;
-UPDATE evento SET imagem = 'http://www.joaoalberto.com/wp-content/uploads/2015/05/22/melhores-do-mundo.jpg' WHERE id = 3;
+UPDATE evento SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2Fimgbarbixas-1.jpg?alt=media&token=fae5beab-aa99-42e9-a9a4-d6c23d6f6b6d' WHERE id = 1;
+UPDATE evento SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F20170818184619323159u.jpg?alt=media&token=eeffc0a8-2486-4e57-9cbd-63178d40dc6e' WHERE id = 2;
+UPDATE evento SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2Fdownload.jpg?alt=media&token=6aaa5150-3f60-4f16-a447-34d3e6544ece' WHERE id = 3;
--- src/main/resources/db/migration/V10__update_table.sql
+++ src/main/resources/db/migration/V10__update_table.sql
@@ -1,1 +1,7 @@
 ALTER TABLE local add column imagem VARCHAR(400);
+UPDATE local SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F1.jpeg?alt=media&token=0327fc3a-b7d6-4995-a435-656cff9e0595' WHERE id = 1;
+UPDATE local SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F2.jpg?alt=media&token=e5a4b679-cc25-4b70-bd26-661043e74102' WHERE id = 2;
+UPDATE local SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F3.jpg?alt=media&token=859f2f9e-3bcd-4603-8d8e-0433beeff008' WHERE id = 3;
+UPDATE local SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F4.jpg?alt=media&token=cacccbd2-34d1-4498-906e-d96f25daa301' WHERE id = 4;
+UPDATE local SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F5.jpg?alt=media&token=079d04dc-f481-4ec5-b43c-84e0cb644b9a' WHERE id = 5;
+UPDATE local SET imagem = 'https://firebasestorage.googleapis.com/v0/b/today-7b8b5.appspot.com/o/images%2F6.jpg?alt=media&token=a07465ba-eed5-4241-83c8-6c4ff13cea95' WHERE id = 6;
